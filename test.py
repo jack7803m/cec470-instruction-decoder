36f8c5780151466b92d396accb8db28e2b67c9e8
Jack Morris <jack@jackmorris.me>
2023-12-03 22:24:16 -0500

    Updated python tester for better debugging

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# Simple program to test whether two files are identical
+# Simple program to test whether two files are identical and print the location of the differences
 # Usage: python test.py <file1> <file2>
 
 import sys
@@ -11,11 +11,21 @@ def main():
     file1 = sys.argv[1]
     file2 = sys.argv[2]
 
+    passed = True
+
     with open(file1) as f1, open(file2) as f2:
-        if f1.read() == f2.read():
-            print("Test passed ✅")
-        else:
-            print("Test failed ❌")
+        for i, (line1, line2) in enumerate(zip(f1, f2)):
+            if line1 != line2:
+                passed = False
+                print("----------------------------------------")
+                print("Test failed ❌")
+                print(f"Difference found on line {i + 1}\n")
+                print(f"File 1: {line1.rstrip()}")
+                print(f"File 2: {line2.rstrip()}")
+
+    if passed:
+        print("Test passed ✅")
+
 
 if __name__ == "__main__":
     main()
